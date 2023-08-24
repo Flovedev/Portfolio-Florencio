a3babdeee5d6f0af088c22c2f5614307ee0b0665
Flovedev <flove.dev@gmail.com>
2023-08-24 12:38:32 +0100

    small fixes

--- src/components/menu/Navbar.tsx
+++ src/components/menu/Navbar.tsx
@@ -2,8 +2,8 @@ import { Navbar, Nav, Button } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <Navbar className="pt-5 mx-4" bg="light" expand="lg">
-      <Navbar.Brand href="#home">Flo</Navbar.Brand>
+    <Navbar className="bg-transparent pt-5 mx-4" bg="light" expand="lg">
+      <Navbar.Brand>Flo</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
